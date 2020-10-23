7b248f4ba7171684638e891122d44132c2ae16d8
Hourann <tdkgn@outlook.com>
2020-10-23 12:27:45 +0800

    [hourann] feat: improve add product page style

--- front-end/src/AddProductPage.js
+++ front-end/src/AddProductPage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import { Button, message, Input } from "antd";
 import Api from "./Api";
+
+import "./addProductPage.css";
 
 export default class AddProductPage extends Component {
   state = {
@@ -8,15 +11,20 @@ export default class AddProductPage extends Component {
     unit: "",
     image: "",
   };
-  onSubmit = async (event) => {
+  onSubmit = async event => {
     event.preventDefault();
-    await Api.post("/product", {
-      price: Math.round(this.state.price * 100),
-      ...this.state,
-    });
+    try {
+      await Api.post("/product", {
+        price: Math.round(this.state.price * 100),
+        ...this.state,
+      });
+      message.success("提交成功");
+    } catch (e) {
+      message.error("提交失败");
+    }
   };
 
-  onFormChange = (event) => {
+  onFormChange = event => {
     const value =
       event.target.name === "price"
         ? Number(Number(event.target.value).toFixed(2))
@@ -29,46 +37,46 @@ export default class AddProductPage extends Component {
     const { name, price, unit, image } = this.state;
     const disableButton = !(name && price && unit && image);
     return (
-      <div className="page">
+      <div className='page add-product-page'>
         <h1>添加商品</h1>
-        <label htmlFor="name">名称：</label>
-        <input
-          type="text"
-          placeholder="名称"
-          name="name"
-          id="name"
+        <label htmlFor='name'>名称：</label>
+        <Input
+          type='text'
+          placeholder='名称'
+          name='name'
+          id='name'
           onChange={this.onFormChange}
         />
-        <label htmlFor="price">价格：</label>
-        <input
-          type="number"
-          placeholder="价格"
-          name="price"
-          id="price"
-          min="0.00"
-          step="0.01"
+        <label htmlFor='price'>价格：</label>
+        <Input
+          type='number'
+          placeholder='价格'
+          name='price'
+          id='price'
+          min='0.00'
+          step='0.01'
           value={this.state.price}
           onChange={this.onFormChange}
         />
-        <label htmlFor="unit">单位：</label>
-        <input
-          type="text"
-          placeholder="单位"
-          name="unit"
-          id="unit"
+        <label htmlFor='unit'>单位：</label>
+        <Input
+          type='text'
+          placeholder='单位'
+          name='unit'
+          id='unit'
           onChange={this.onFormChange}
         />
-        <label htmlFor="image">图片：</label>
-        <input
-          type="text"
-          placeholder="URL"
-          name="image"
-          id="image"
+        <label htmlFor='image'>图片：</label>
+        <Input
+          type='text'
+          placeholder='URL'
+          name='image'
+          id='image'
           onChange={this.onFormChange}
         />
-        <button disabled={disableButton} onClick={this.onSubmit}>
+        <Button type='primary' disabled={disableButton} onClick={this.onSubmit}>
           提交
-        </button>
+        </Button>
       </div>
     );
   }
