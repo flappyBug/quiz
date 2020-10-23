b2cdd11a0c9a21714fc2b2bb0216a31e1d3b1d5a
Hourann <tdkgn@outlook.com>
2020-10-23 10:52:48 +0800

    [hourann] feat: improve product style

--- front-end/src/Product.js
+++ front-end/src/Product.js
@@ -1,19 +1,21 @@
 import React from "react";
-
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import "./product.css";
 const Product = ({ name, price, unit, image, onAddToCart }) => (
-  <div className="product">
+  <div className='product'>
     <img src={image} alt={name} />
-    <p>{name}</p>
-    <p className="desc">
-      单价：{price}元/{unit}
-    </p>
-    <button
-      className="button-add"
-      onClick={() => onAddToCart({ name, price, unit, image })}
-    >
-      +
-    </button>
+    <div className='desc'>
+      <p>{name}</p>
+      <p className='price'>
+        单价：{price}元/{unit}
+      </p>
+      <Button
+        icon={<PlusOutlined />}
+        shape='circle'
+        onClick={() => onAddToCart({ name, price, unit, image })}
+      />
+    </div>
   </div>
 );
 export default Product;
